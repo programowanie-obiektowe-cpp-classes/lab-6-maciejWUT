49762cdd044cebd57dc7c12ae34986830050f5ab
maciejWUT <mciek.sieradzki@gmail.com>
2022-12-20 13:05:38 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,15 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+        std::for_each(std::begin(people), std::end(people), [&](Human& human) {
+        if (human.isMonster())
+            ret_v.push_back('n');
+        else
+            ret_v.push_back('y');
+        human.birthday();
+    });
+
+    reverse(ret_v.begin(), ret_v.end());
 
     return ret_v;
 }
